947229678080c7b645464af3fadab57d4bc7853e
vitali-usik <vitaliy.usik@idt.net>
2019-02-20 17:19:55 +0300

    add renderFilters function

---
+++ src/main.js
@@ -0,0 +1,85 @@
+'use strict';
+
+let mainFilterElement = document.getElementsByClassName(`main__filter`)[0];
+
+let mockFilterData = {
+  'all': {
+    name: `all`,
+    count: 15,
+    id: `all`,
+    isChecked: true,
+  },
+  'overdue': {
+    name: `overdue`,
+    count: 0,
+    id: `overdue`,
+    isDisabled: true,
+  },
+  'today': {
+    name: `today`,
+    count: 0,
+    id: `today`,
+    isDisabled: true,
+  },
+  'favorites': {
+    name: `favorites`,
+    count: 7,
+    id: `favorites`,
+  },
+  'repeating': {
+    name: `repeating`,
+    count: 2,
+    id: `repeating`,
+  },
+  'tags': {
+    name: `tags`,
+    count: 6,
+    id: `tags`,
+  },
+  'archive': {
+    name: `archive`,
+    count: 115,
+    id: `archive`,
+  }
+};
+
+const clearFiltersContainer = () => {
+  mainFilterElement.innerHTML = ``;
+};
+
+const renderFilter = ({name, count, id, isChecked = false, isDisabled = false}) => {
+  const state = isChecked
+    ? `checked`
+    : isDisabled ? `disabled` : ``;
+
+  return `
+    <input
+      type="radio"
+      id="filter__${id}"
+      class="filter__input visually-hidden"
+      name="filter"
+      ${state}
+    />
+    <label for="filter__${id}" class="filter__label">
+      ${name} <span class="filter__${id}-count">${count}</span>
+    </label>
+  `;
+};
+
+const renderFilters = (data) => {
+  let fragment = ``;
+
+  for (let filter in data) {
+    fragment += renderFilter(data[filter]);
+  }
+
+  mainFilterElement.insertAdjacentHTML(`beforeend`, fragment);
+};
+
+function init() {
+  clearFiltersContainer();
+  renderFilters(mockFilterData);
+  // console.log(`it works! ${renderFilter('test', 666, 'all', false)}`);
+}
+
+init();
